c84039ca4ab3ba3910e7eb2ddd3425d72ecbd317
Thao <thaolethac2121@gmail.com>
2022-11-23 16:17:56 +0700

    Add project files.

---
+++ JwtWebApiTutorial/User.cs
@@ -0,0 +1,11 @@
+﻿    namespace JwtWebApiTutorial
+{
+    public class User
+    {
+        public string Username = string.Empty;
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
+    }
+}
